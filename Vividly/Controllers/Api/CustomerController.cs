657beccedb2232e14f395f2d010fc747e757b432
dranzture <polatcoban@gmail.com>
2019-05-06 14:12:10 -0500

    web apis - data transfer objects

---
+++ Vividly/Controllers/Api/CustomerController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vividly.Models;
+
+namespace Vividly.Controllers.Api
+{
+    public class CustomerController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/customer
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customer/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.ID == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return customer;
+        }
+        // POST /api/customer
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return customer;
+        }
+
+        // PUT /api/customer/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.ID == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            customerInDb.FirstName = customer.FirstName;
+            customerInDb.LastName = customer.LastName;
+            customerInDb.DateOfBirth = customer.DateOfBirth;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeID = customer.MembershipTypeID;
+
+            _context.SaveChanges();
+        }
+        // DELETE /api/customer/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.ID == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Vividly/DTOs/CustomerDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vividly.Models;
+
+namespace Vividly.DTOs
+{
+    public class CustomerDTO
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        public byte MembershipTypeID { get; set; }
+    }
+}
